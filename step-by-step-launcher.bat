d46a32abd5fd87ebc4aff53e31d3e9046b659675
CrisisRogue <37817536+CrisisRogue@users.noreply.github.com>
2018-04-08 16:12:24 -0400

    Add files via upload

---
+++ step-by-step-launcher.bat
@@ -0,0 +1,47 @@
+@echo off
+
+cls
+echo Hi, and welcome to the Step-by-step guide on how to mine and use UaiCoin! 
+echo.
+echo.
+pause
+
+cls
+echo We will start by starting the miner.
+echo .
+echo .
+start cmd.exe
+echo Start it by typing "miner --address [your address here]"
+pause
+
+cls
+echo The miner is currently mining if you did this correctly, if it doesn't mine ( you know this if your performance is still good and your temperatures are low ) join opur discord ( https://discord.gg/r6FNtpE ) for more help.
+echo .
+echo .
+echo Now, press the "X" of the miner window.
+pause
+
+cls
+echo Next: The wallet.
+start simplewallet.exe
+echo To create your wallet, press "G" on your keyboard.
+echo .
+echo You will have to type a name for your wallet. DO NOT FORGET THAT NAME!!!
+pause
+
+cls
+echo .
+start Backup-Maker.bat
+echo Type "create" to create a password to your wallet backup, where will be stored your private spend key, your view key and your phrase. 
+echo .
+echo .
+echo  Type the password of your choice. DO NOT FORGET THE PASSWORD YOU TYPE THERE!!!
+pause
+
+cls
+echo Follow the instructions that the Backup maker gives you.
+pause
+
+cls
+echo You have completed the guide! Thanks for joining us in our journey!
+pause
